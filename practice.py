49d53bb95437dacfdf1e28f3519d64e5c057f13f
sss072 <samsabeeh6@gmail.com>
2022-01-02 23:32:16 -0500

    6kyupractice3'

--- practice.py
+++ practice.py
@@ -54,3 +54,24 @@ def alphabet_position(text):
     string_ints = [str(int) for int in text]
     return ' '.join(string_ints)
             
+
+
+# Your task is to sort a given string. Each word in the string will contain a single number.
+#  This number is the position the word should have in the result.
+
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+# If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+def order(sentence):
+    new = sentence.split()
+    result = new.copy()
+    for x in new:
+        word_list = list(x)
+        for x in word_list:
+            if x.isdigit():
+                word_list.remove(x)
+                result[int(x)-1] = ''.join(word_list)
+    return ' '.join(result)
+    
+order("is2 Thi1s T4est 3a")
