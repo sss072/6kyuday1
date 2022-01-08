86b43f8f864591ccfd2813f4fd8df76ad5ad83cb
sss072 <samsabeeh6@gmail.com>
2022-01-07 23:24:01 -0500

    'bfasdjkfhkadsjhf'

--- practice.py
+++ practice.py
@@ -101,3 +101,27 @@ def longest(a1, a2):
             result.append(x)
     result.sort()
     return ''.join(result)
+
+
+# Trolls are attacking your comment section!
+
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+# Note: for this kata y isn't considered a vowel.
+
+def disemvowel(string_):
+    compare = ['a','e','i','o','u']
+    string_ = list(string_)
+    new = ['1' if x.lower() in compare else x for x in string_]
+    final = []
+    for x in new:
+        if x != '1':
+            final.append(x)
+    final = ''.join(final)  
+    
+    
+    return final
