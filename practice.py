7b98b4c8f9befa8e95c1f9c0654acb7c3b5e9be5
sss072 <samsabeeh6@gmail.com>
2022-01-16 23:54:18 -0500

    'sadfjahdsf'

--- practice.py
+++ practice.py
@@ -208,3 +208,28 @@ def sort_array(source_array):
             source_array[i] = copy[counter]
             counter += 1
     return source_array
+
+
+# Given two arrays a and b write a function
+#  comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the 
+#  "same" elements, with the same multiplicities (the multiplicity of a member 
+#  is the number of times it appears). "Same" means, here, that the elements in b
+#   are the elements in a squared, regardless of the order.
+
+    def comp(array1, array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
+        return False
+
+
+# is a number prime
+def is_prime(num):
+    if num <= 0 or num == 1:
+        return False
+    i = 2
+    while (i <= num ** 0.5 ):
+        if num % i == 0:
+            return False
+        i += 1
+    return True
