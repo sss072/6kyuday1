2cf486eb1b31fa3a3c5937135840e50462db46d3
sss072 <samsabeeh6@gmail.com>
2022-01-25 21:33:04 -0500

    'sadfznmcx'

--- practice.py
+++ practice.py
@@ -316,7 +316,15 @@ def sum_dig_pow(a, b):
 # Put together these modified chunks and return the result as a string.
 
 
+# Write a function that takes an array of numbers (integers for the tests) and a target number. 
+# It should find two different items in the array that, when added together, give the target value. 
+# The indices of these items should then be returned in a tuple like so: (index1, index2).
 
+# For the purposes of this kata, some tests may have multiple answers; any valid solutions will be 
+# accepted.
+
+# The input will always be valid (numbers will be an array of length 2 or greater, and all of the 
+# items will be numbers; target will always be the sum of two different items from that array).
 
 def revrot(strng, sz):
     if sz == 0 or sz > len(strng) or strng == '':
@@ -351,3 +359,11 @@ def camel_case(string):
     for x in range(len(string)):
         string[x] = string[x].capitalize()
     return ''.join(string)
+
+
+
+def two_sum(numbers, target):
+    for x in range(len(numbers)-1):
+        for y in range(x+1,len(numbers)):
+            if numbers[x] + numbers[y] == target:
+                return (x,y)
